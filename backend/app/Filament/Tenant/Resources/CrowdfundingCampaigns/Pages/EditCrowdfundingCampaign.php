1ee5f9cd353d2d1e5873241fe4003ec3b77b5c05
bozz333 <alain.francis123@gmail.com>
2026-05-20 13:10:47 +0000

    refactor: restore crowdfunding tenant resource

---
+++ backend/app/Filament/Tenant/Resources/CrowdfundingCampaigns/Pages/EditCrowdfundingCampaign.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Tenant\Resources\CrowdfundingCampaigns\Pages;
+
+use App\Filament\Support\Pages\EditRecordPage;
+use App\Filament\Tenant\Resources\CrowdfundingCampaigns\CrowdfundingCampaignResource;
+use Filament\Support\Enums\Width;
+
+class EditCrowdfundingCampaign extends EditRecordPage
+{
+    protected static string $resource = CrowdfundingCampaignResource::class;
+
+    protected Width|string|null $maxContentWidth = Width::Full;
+}
